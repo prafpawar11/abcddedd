6ac5fe06c490dc4827d2c68a5ad49239157e105c
Suraj Patil <prafpawar11@gmail.com>
2023-03-14 20:19:48 +0530

    Framework created

---
+++ src/test/java/TestLayer/LoginPage.java
@@ -0,0 +1,18 @@
+package TestLayer;
+
+public class LoginPage {
+
+	public void loginFuctionality()
+	{
+		System.out.println("Login Functionality");
+		
+	}
+	
+	public static void main(String[] args) {
+		LoginPage obj =new LoginPage();
+		
+		obj.loginFuctionality();
+	}
+	
+	
+}
